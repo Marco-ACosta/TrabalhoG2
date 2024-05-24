b7e64c3047d5c1ae5921eb8e28b21ef334d2655c
CairoLopess <106634884+CairoLopess@users.noreply.github.com>
2024-05-23 22:10:43 -0300

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useAuthContext } from "../context/AuthContext";
 
 export default function Home() {
@@ -8,6 +9,14 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-800 py-6 flex flex-col justify-center sm:py-12">
       <h1 className="text-3xl font-bold leading-relaxed text-gray-100 text-center">Bem vindo {userAuth?.email} ao sistema de Metas/Tarefas</h1>
+      
+      {(!userAuth?.uid && 
+      <Link
+        className="text-white hover:text-gray-300 transition-colors duration-300 cursor-pointer text-center"
+        href="/signin"
+      >
+        Fazer Login
+      </Link>)}
     </div>
   );
   
